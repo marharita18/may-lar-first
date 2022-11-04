b48a7cfcba3e7b54b49877f89139bf152fbaa369
marharyta18 <margolu18@gmai.com>
2022-11-04 16:29:45 +0100

    view change action

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -5,7 +5,7 @@
         <!-- Отображение ошибок проверки ввода -->
         @include('common.errors')
         <!-- Форма новой задачи -->
-        <form action="{{ url('task') }}" method="POST" class="form-horizontal">
+        <form action="{{ route('task.store') }}" method="POST" class="form-horizontal">
             {{ csrf_field() }}
 
             <!-- Имя задачи -->
@@ -13,7 +13,7 @@
                 <label for="task" class="col-sm-3 control-label">Задача</label>
 
                 <div class="col-sm-6">
-                    <input type="text" name="name" id="task-name" class="form-control">
+                    <input type="text" name="name" id="task" class="form-control" value="{{old('name')}}">
                 </div>
             </div>
 
